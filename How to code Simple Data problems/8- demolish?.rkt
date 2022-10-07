dc3e92938942d65b7e714cc488a81b936f6b646f
Madoo Mino <39817130+madoomino@users.noreply.github.com>
2022-10-07 16:57:43 +0200

    Create 8- demolish?.rkt

---
+++ How to code Simple Data problems/8- demolish?.rkt
@@ -0,0 +1,63 @@
+;; demolish-starter.rkt
+
+;; =================
+;; Data definitions:
+
+; 
+; PROBLEM A:
+; 
+; You are assigned to develop a system that will classify 
+; buildings in downtown Vancouver based on how old they are. 
+; According to city guidelines, there are three different classification levels:
+; new, old, and heritage.
+; 
+; Design a data definition to represent these classification levels. 
+; Call it BuildingStatus.
+; 
+
+
+;; BuildingStatus is one of:
+;; - new
+;; - old
+;; - heritage
+
+;; 
+
+#;
+(define (fn-for-building-status bs)
+  (cond [(string=? "new") (...)]
+        [(string=? "old") (...)]
+        [(string=? "heritage") (...)]))
+
+;; Template rules used:
+;; one of 3 cases:
+;; atomic-distinct: "new
+;; atomic-distinct: "old"
+;; atomic-distinct: "heritage"
+
+
+;; =================
+;; Functions:
+
+; 
+; PROBLEM B:
+; 
+; The city wants to demolish all buildings classified as "old".
+; You are hired to design a function called demolish? 
+; that determines whether a building should be torn down or not.
+; 
+
+
+;; BuildingStatus -> Boolean
+;; determines whether a building should be torn down or not
+
+(check-expect (demolish? "old") true)
+(check-expect (demolish? "new") false)
+(check-expect (demolish? "heritage") false)
+
+;(define (demolish? bs) false) ;stub
+
+;<use template from BuildingStatus>
+
+(define (demolish? bs)
+  (string=? bs "old"))
